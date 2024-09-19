b0afbc289316c9e7db1ed27ccfcdf67bd46f37a0
Diego <diegolima.almeida93@gmail.com>
2024-09-19 18:19:23 -0300

    feat: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import "./styles/header.scss";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <header className="header">
+        <Image src="/logo.png" alt="Logo do site" width="150" height="36" />
+        <h1>Bem-vindo de volta, Marcus</h1>
+        <span>Segunda, 01 de dezembro de 2025</span>
+      </header>
+    </>
+  );
 }
